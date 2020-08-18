6ccd7295e264081d197879accf8c393da723f3d6
hsinhuibiga <hsinhuibiga@gmail.com>
2020-08-18 14:52:09 +1000

    Add files via upload

---
+++ Valid Parentheses.py
@@ -0,0 +1,17 @@
+#Valid Parentheses
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        pars = [None]
+        parmap = {')': '(', '}': '{', ']': '['}
+        for c in s:
+            if c in parmap:
+                if parmap[c] != pars.pop():
+                    return False
+            else:
+                pars.append(c)
+        return len(pars) == 1
