80f17df24e5f344938966b3e82257a0bb12c0a4b
hsinhuibiga <hsinhuibiga@gmail.com>
2020-08-18 12:18:17 +1000

    Add files via upload

---
+++ Hamming.py
@@ -0,0 +1,15 @@
+#Hamming Distance
+
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        count = 0
+        xor = x ^ y
+        while xor != 0:
+            xor &= (xor - 1)
+            count += 1
+        return count
