56d51002507f69e0792366a2145b11473468cc4d
hsinhuibiga <hsinhuibiga@gmail.com>
2020-08-21 20:43:18 +1000

    Add files via upload

---
+++ Sort Array By Parity.py
@@ -0,0 +1,9 @@
+#Sort Array By Parity
+
+class Solution(object):
+    def sortArrayByParity(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        return sorted(A, key = lambda x : x % 2)
